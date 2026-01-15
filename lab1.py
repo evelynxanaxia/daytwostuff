c9672d162cfcefbcb682bd79e5ff962a7cc25457
evelynxanaxia <evelynxanixa@icloud.com>
2026-01-15 21:55:10 +0000

    several changes to your environment, including adding a python file, dataset and updates to the requirements file

---
+++ lab1.py
@@ -0,0 +1,10 @@
+# %%
+#/workspaces/daytwostuff #firstone
+#/workspaces/daytwostuff #secondone
+# %%
+print("Hello, World!")
+# %%
+import pandas as pd
+data = pd.read_csv('F1.csv')
+print(data.head())
+# %%
